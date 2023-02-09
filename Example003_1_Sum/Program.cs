478a2d396daa906e0981b88a13e3d116f40e6fa1
Sergei Sh <sergshrb@mail.ru>
2023-02-09 20:21:59 +0300

    add random

---
+++ Example003_1_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿// Программа сложения 2х случайных целых чисел
+int numberA = new Random().Next(1,10); // 1 2 3 4 ...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10); // 1 2 3 4 ...9
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);

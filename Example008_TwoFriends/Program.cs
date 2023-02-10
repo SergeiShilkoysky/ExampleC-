9e6ff61bfc17685072fdf2f0ea4eb79f5ea687e5
Sergei Sh <sergshrb@mail.ru>
2023-02-10 12:54:32 +0300

    Example 008 two friends and dogs

---
+++ Example008_TwoFriends/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача про 2х друзей
+Console.Clear();
+
+int a = 1; // скорость первого друга 
+int b = 2; // скорость второго друга
+int c = 5; // скорость собаки 
+bool f = true; // собака бежит ко второму другу
+int d = 10000; // дистанция между друзьями в метрах
+int count = 0;
+int t = 0;
+
+while(d > 10)
+{
+    if(f == false)
+    {
+    t = d / (a + c);
+    f = true;
+    }
+    else
+    {
+    t = d / (b + c);
+    f = false;
+    }
+    d = d - (a + b) * t;
+    count++;
+}
+Console.Write("Собака пробежала количество раз: ");
+Console.WriteLine(count);

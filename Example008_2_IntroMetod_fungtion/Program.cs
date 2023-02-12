6f08a013f450ce3da02f3228a8118ca2653ad35a
Sergei Sh <sergshrb@mail.ru>
2023-02-12 23:39:42 +0300

    поиск мах из 9 чисел ч/з функцию

--- Example008_2_IntroMetod_fungtion/Program.cs
+++ Example008_2_IntroMetod_fungtion/Program.cs
@@ -14,11 +14,14 @@ int b2 = 23;
 int c2 = 33;
 int a3 = 13;
 int b3 = 23;
-int c3 = -333;
+int c3 = 78;
 
-int max1 = Max(a1, b1, c1); // т.е. максимум из 3х
-int max2 = Max(a2, b2, c2); 
-int max3 = Max(a3, b3, c3); 
-int max = Max(max1, max2, max3);               // финальное сравнение
+//int max1 = Max(a1, b1, c1); // т.е. максимум из 3х
+//int max2 = Max(a2, b2, c2); 
+//int max3 = Max(a3, b3, c3); 
+//int max = Max(max1, max2, max3);               // финальное сравнение
+// еще улучшить:
+int max = Max (Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
 Console.WriteLine(max);
 
